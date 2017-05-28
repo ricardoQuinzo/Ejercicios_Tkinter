f92cf296b78efac0b3982e2327e65925fc57b6cc
DESKTOP-SRK6ME2\Ricardo <ricardoquinzo@hotmail.com>
2017-05-28 17:53:18 -0500

    correccion del ejercicio 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,5 +1,7 @@
 from tkinter import *
 root = Tk()
-etiqueta1 = Label(root, text="Hello Tkinter!")
+etiqueta1 = Label(root, text="Escuela politecniaca nacional")
+etiqueta2 = Label(root, text="Programacion Avanzada")
 etiqueta1.pack()
+etiqueta2.pack()
 root.mainloop()
