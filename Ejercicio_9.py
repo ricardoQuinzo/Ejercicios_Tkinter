a0acb990f6fcc83b452eafdb0cbc08f36e9c0bbc
Estefania-PC\Estefania <danielflorestamayo@gmail.com>
2017-05-29 16:16:19 -0500

    Ejercicio_9_Correción

--- Ejercicio_9.py
+++ Ejercicio_9.py
@@ -1,8 +1,8 @@
 from Tkinter import*
 
 master = Tk()
-Label(master, text ="Primer Nombre").grid(row=0)
-Label(master, text ="Primer Nombre").grid(row=1)
+Label(master, text ="First Name: ").grid(row=0)
+Label(master, text ="Last Name: ").grid(row=1)
 
 e1 = Entry(master)
 e2 = Entry(master)
